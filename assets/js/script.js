872ff25b995742224e0e96942273aaacd2115bc2
Nicoleta Constantin <constantinnicoleta@hotmail.com>
2024-02-16 00:31:03 +0000

    add function to display end of quiz and score , add picture for end of quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,39 +7,39 @@ document.addEventListener("DOMContentLoaded", function () {
     //event listener for the play-again button
     const playAgainButton = document.getElementById("play-again-btn");
     playAgainButton.addEventListener("click", restartQuiz);
-}); 
+});
 
 //Array to hold the options and correct answers
 const princesses = [{
-    imageUrl: "assets/images/cinderella.png",
-    options: ["Tina", "Cindarella", "Mulan"],
-    answer: "Cindarella"
-},
-{
-    imageUrl: "assets/images/merida.png",
-    options: ["Merida", "Tiana", "Anna"],
-    answer: "Merida"
-},
-{
-    imageUrl: "assets/images/tiana.png",
-    options: ["Ariel", "Tiana", "Jasmine"],
-    answer: "Tiana"
-},
-{
-    imageUrl: "assets/images/mulan.png",
-    options: ["Rapunzel", "Mulan", "Moana"],
-    answer: "Mulan"
-},
-{
-    imageUrl: "assets/images/anna.png",
-    options: ["Merida", "Anna", "Belle"],
-    answer: "Anna"
-},
-{
-    imageUrl: "assets/images/elsa.png",
-    options: ["Aurora", "Snow White", "Elsa"],
-    answer: "Elsa"
-},
+        imageUrl: "assets/images/cinderella.png",
+        options: ["Tina", "Cindarella", "Mulan"],
+        answer: "Cindarella"
+    },
+    {
+        imageUrl: "assets/images/merida.png",
+        options: ["Merida", "Tiana", "Anna"],
+        answer: "Merida"
+    },
+    {
+        imageUrl: "assets/images/tiana.png",
+        options: ["Ariel", "Tiana", "Jasmine"],
+        answer: "Tiana"
+    },
+    {
+        imageUrl: "assets/images/mulan.png",
+        options: ["Rapunzel", "Mulan", "Moana"],
+        answer: "Mulan"
+    },
+    {
+        imageUrl: "assets/images/anna.png",
+        options: ["Merida", "Anna", "Belle"],
+        answer: "Anna"
+    },
+    {
+        imageUrl: "assets/images/elsa.png",
+        options: ["Aurora", "Snow White", "Elsa"],
+        answer: "Elsa"
+    },
 ];
 
 let shuffledPrincesses = [];
@@ -54,7 +54,7 @@ const questionElement = document.getElementById("question");
 const optionsContainer = document.getElementById("options-container");
 const scoreElement = document.getElementById("score");
 
-scoreElement.style.display = "none"; 
+scoreElement.style.display = "none";
 
 // Function to shuffle the array
 /**
@@ -141,4 +141,15 @@ function checkAnswer(selectedOption) {
             document.getElementById("play-again-btn").style.display = "block";
         }, 1000);
     }
+}
+
+//function to display end of quiz and score
+function showResult() {
+    princessImageElement.src = "assets/images/belle.png";
+    questionElement.textContent = "Quiz Completed!";
+    optionsContainer.innerHTML = "";
+    scoreElement.textContent = `Your Score: ${score} out of ${shuffledPrincesses.length}`;
+    scoreElement.style.display = "block";
+    document.getElementById("action-button").style.display = "none";
+    document.getElementById("play-again-btn").style.display = "block";
 }
