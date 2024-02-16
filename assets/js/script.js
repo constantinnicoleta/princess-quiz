c5cab5ac6263e722b5323ac09686b4432aa8a164
Nicoleta Constantin <constantinnicoleta@hotmail.com>
2024-02-16 00:05:42 +0000

    add array to hold the options & answers to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,36 @@ document.addEventListener("DOMContentLoaded", function () {
     const playAgainButton = document.getElementById("play-again-btn");
     playAgainButton.addEventListener("click", restartQuiz);
 }); 
+
+//Array to hold the options and correct answers
+const princesses = [{
+    imageUrl: "assets/images/cinderella.png",
+    options: ["Tina", "Cindarella", "Mulan"],
+    answer: "Cindarella"
+},
+{
+    imageUrl: "assets/images/merida.png",
+    options: ["Merida", "Tiana", "Anna"],
+    answer: "Merida"
+},
+{
+    imageUrl: "assets/images/tiana.png",
+    options: ["Ariel", "Tiana", "Jasmine"],
+    answer: "Tiana"
+},
+{
+    imageUrl: "assets/images/mulan.png",
+    options: ["Rapunzel", "Mulan", "Moana"],
+    answer: "Mulan"
+},
+{
+    imageUrl: "assets/images/anna.png",
+    options: ["Merida", "Anna", "Belle"],
+    answer: "Anna"
+},
+{
+    imageUrl: "assets/images/elsa.png",
+    options: ["Aurora", "Snow White", "Elsa"],
+    answer: "Elsa"
+},
+];
